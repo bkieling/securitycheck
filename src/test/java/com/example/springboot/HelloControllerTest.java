3433aaf7266337bbd41af9b52f999d09e3734e92
Melissa Ugalde <melissau@vmware.com>
2022-05-10 15:52:51 +0100

    change 2

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -22,11 +22,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu + WeB fleet Security check!", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu + WeB fleet Security check change2 !", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu + WeB fleet Security check!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu + WeB fleet Security check change2 !"));
     }
 }
