c11f93f91f19cfe074bb225e968bccc8bca04a9b
fcaroline <fcaroline@vmware.com>
2022-05-11 10:13:08 +0200

    testing GCR

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -22,11 +22,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu + WeB fleet Security check change!", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu + WeB fleet Security check change test!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu + WeB fleet Security check change!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu + WeB fleet Security check change test!"));
     }
 }
